c19b2eb8b47196cb332e5c4dacad2c30f90e6f8d
GIS <dev@gis4biz.ru>
2023-05-23 11:03:06 +0300

    fix admin pill for config models

--- src/resources/views/admin/block-groups/includes/pill.blade.php
+++ src/resources/views/admin/block-groups/includes/pill.blade.php
@@ -1,5 +1,5 @@
-@foreach(config("site-blocks.models") as $title => $model)
-    @if ($model == "App\Page")
+@foreach(config("site-blocks.models") as $title => $class)
+    @if ($title == $model)
         <li class="nav-item">
             <a href="{{ route("admin.blocks.groups.model", ["model" => $model, "model_id" => $modelId]) }}"
                class="nav-link{{ $currentRoute === "admin.blocks.groups.model" ? " active" : "" }}">
